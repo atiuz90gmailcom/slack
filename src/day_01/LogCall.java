cc524b5a5394e4787b8a6fc0951e3bbe5c30ce94
atiuz90gmailcom <atiuz90@gmail.com>
2020-07-25 19:01:08 -0400

    LogCall.java

--- src/day_01/LogCall.java
+++ src/day_01/LogCall.java
@@ -4,6 +4,8 @@ public class LogCall {
 
     public static void main(String[] args) {
         System.out.println("__________________________");
+
+        ////////////////
     }
 
     // users are able to call through slack
