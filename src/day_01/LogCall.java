4e5b59e8a948bce5fbd13fb5afc3e17656c41e8a
atiuz90gmailcom <atiuz90@gmail.com>
2020-07-25 18:54:10 -0400

    add LogCall.java

--- src/day_01/LogCall.java
+++ src/day_01/LogCall.java
@@ -2,6 +2,10 @@ package day_01;
 
 public class LogCall {
 
+    public static void main(String[] args) {
+        System.out.println("__________________________");
+    }
+
     // users are able to call through slack
     
     // this line has been made in GitHub
